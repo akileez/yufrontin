3f3930611eeaa173766f3c5820a4e20439d646fd
akileez <git@keeda.net>
2015-09-16 19:57:04 -0400

    source code upload

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-/*!
- * frontin <https://github.com/akileez/frontin>
- *
- * Copyright (c) 2015 Keith Williams.
- * Licensed under the ISC license.
- */
+//  yufrontin <https://github.com/akileez/yufrontin>
+//  Copyright (c) 2015 Keith Williams.
+//  Licensed under the ISC license.
 
-'use strict'
+// adopted from: front-matter-extractor <https://github.com/75lb/front-matter-extractor>
+// using yaml.js <https://github.com/jeremyfa/yaml.js>
 
-module.exports = function () {
-  // do stuff
+var yaml = require('./lib/Yaml')
+
+function extract (input) {
+  var matter = /^---$([\s\S]*)^---\s/m
+
+  var result = {
+    data: {},
+    content: input
+  }
+
+  if (input && typeof input === 'string') {
+    var matches = input.match(matter)
+
+    if (matches) {
+      // js-yaml implementation:
+      // result.data = yaml.safeLoad(matches[1])
+
+      // yamljs implementation:
+      result.data = yaml.parse(matches[1])
+
+      result.content = input.replace(matches[0], '')
+    }
+  }
+
+  return result
 }
+
+module.exports = extract
